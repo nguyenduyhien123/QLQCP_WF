3d0dce2a0a723efc153072bc1782fbf70c0bf8e4
nguyenduyhien123 <93326941+nguyenduyhien123@users.noreply.github.com>
2023-10-18 00:43:02 +0700

    update code

---
+++ 9_12_QuanLyQuanCaPhe.cs
@@ -0,0 +1,57 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+namespace _9_12_QuanLyQuanCaPhe
+{
+    internal class _9_12_QuanLyQuanCaPhe
+    {
+        public string account = "";
+        SqlConnection con = new SqlConnection();
+        void KetNoi()
+        {
+            con.ConnectionString = @"Data source=(localdb)\mssqllocaldb;Initial Catalog=QUANLYQUANCAPHE;integrated Security=True";
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+        }
+        void DongKetNoi()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                //con.Close();
+            }
+        }
+        public _9_12_QuanLyQuanCaPhe()
+        {
+            KetNoi();
+        }
+        ~_9_12_QuanLyQuanCaPhe()
+        {
+            DongKetNoi();
+        }
+        public DataSet LayDuLieu(string sql)
+        {
+            DataSet dataset = new DataSet();
+            //để kết nối db đưa vào dataset dùng SqlDataAdapter 
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            da.Fill(dataset);
+            return dataset;
+        }
+        public int CapNhatDuLieu(string sql)
+        {
+            //con.Open();
+            // Tạo SqlCommand và thực hiện truy vấn UPDATE
+            SqlCommand command = new SqlCommand(sql, con);
+            int affectedRows = command.ExecuteNonQuery(); // Số bản ghi bị ảnh hưởng bởi truy vấn
+            //con.Close();
+            return affectedRows;
+        }
+        public int capnhatdulieu(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            return cmd.ExecuteNonQuery();
+        }
+    }
+
+}
---
+++ clsquanlycaphe.cs
@@ -0,0 +1,46 @@
+﻿//thu vien kett noi database
+using System.Data;
+using System.Data.SqlClient;
+namespace _9_12_QuanLyQuanCaPhe
+{
+    internal class clsquanlycaphe
+    {
+        SqlConnection con = new SqlConnection();
+
+        void ketnoi()
+        {
+            con.ConnectionString = @"Data source=(localdb)\mssqllocaldb;Initial Catalog=QUANLYQUANCAPHE; integrated Security=True";
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+        void dongketnoi()
+        {
+            con.Close();
+        }
+
+        public clsquanlycaphe()
+        {
+            ketnoi();
+        }
+        //dataset chứa nhiều bảng
+        public DataSet LayDuLieu(string sql)
+        {
+            DataSet dataset = new DataSet();
+            //để kết nối db đưa vào dataset dùng SqlDataAdapter 
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            da.Fill(dataset);
+            return dataset;
+        }
+        public int capnhatdulieu(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            return cmd.ExecuteNonQuery();
+        }
+    }
+}
